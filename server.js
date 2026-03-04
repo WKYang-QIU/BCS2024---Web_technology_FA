df2cae7c0b24e1382cf97ca2533ebcb726bd36c3
WKYang-QIU <khaiyang.won@qiu.edu.my>
2026-03-04 18:29:05 +0800

    Initialize Node project and setup basic Express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mysql = require('mysql2')
+require('dotnev').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send("Server running")
+})
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000")
+})
